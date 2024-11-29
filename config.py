a86d5214a7b7346bd812186d33dc980c340ec058
jsa-x <jesus.sanchez14@utrgv.edu>
2024-11-28 19:41:06 -0600

    Update config.py

--- config.py
+++ config.py
@@ -2,9 +2,9 @@ import os
 
 # MySQL credentials
 MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-MYSQL_USER = os.getenv('MYSQL_USER', 'jesus')
-MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'CyberSec@24')
+MYSQL_USER = os.getenv('MYSQL_USER', 'Your_User')
+MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'Your_Password')
 MYSQL_DB = os.getenv('MYSQL_DB', 'keyboard_store')
 
 # Secret key for session management
-SECRET_KEY = os.getenv('SECRET_KEY', 'cybersecurity')
+SECRET_KEY = os.getenv('SECRET_KEY', 'Your_Secret_Key')
